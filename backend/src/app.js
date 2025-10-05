a50578ca6bebfaddb8aa203dfbb136cc3e2f887d
Sakshi <agrawalsag151@gmail.com>
2025-10-05 15:57:07 +0530

    cahngessss

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const app= express();
 
 app.use(cors({
   // origin: 'http://localhost:5173',
-  origin: 'https://sound-stream-application-2c7v.vercel.app/',
+   origin: 'https://sound-stream-application-2c7v.vercel.app',  
   credentials: true
 }))
 
