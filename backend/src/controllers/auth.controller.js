0a7cd4a774d6128b58b917902fcd5b9fc9edb782
Sakshi <agrawalsag151@gmail.com>
2025-10-17 22:34:18 +0530

    deploy4

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,125 @@
+// import userModel from "../models/user.model.js" 
+// import bcrypt from "bcryptjs";
+// import jwt from "jsonwebtoken";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+
+// export async function registerUser(req,res){
+//     const{username,password} = req.body;
+
+//     const isUserExists= await userModel.findOne({username})
+
+//     if(isUserExists){
+//         return res.status(400).json({
+//             message: "User already exists",
+//         })}
+
+//         const hashedPassword = await bcrypt.hash(password, 10);
+
+//         const user=await userModel.create({
+//             username,
+//             password: hashedPassword,
+//         });
+
+//         const token= jwt.sign({
+//             id:user._id},
+//              process.env.JWT_SECRET_KEY,
+//             {expiresIn: "1d"})    //1day
+
+//                  res.cookie("token", token, {
+//             httpOnly: true,
+//             secure: process.env.NODE_ENV === "production", // only secure in production
+//             sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+//             maxAge: 24 * 60 * 60 * 1000,
+//         });
+//             //set cookie in response
+
+//         //Response send krne se phle token generate krege 
+//         return res.status(201).json({
+//             messsage: "User registered successfully",
+//             user: {
+//                 id: user._id,
+//                 username: user.username,
+//             },
+//             token})
+//         }
+
+//             export async function loginUser(req,res){
+//                 const {username,password}=req.body
+                
+//                 const user = await userModel.findOne({username})
+
+//                 if(!user){
+//                     return res.status(400).json({
+//                         message: "Invalid username or password",
+//                     })
+//                 }
+            
+
+//             const isPasswordValid = await bcrypt.compare(password, user.password);
+
+//             if(!isPasswordValid){
+//                 return res.status(400).json({
+//                     message: "Invalid username or password",
+//                 });
+//             }
+
+//             const token = jwt.sign(
+//                 { id: user._id },
+//                 process.env.JWT_SECRET_KEY,
+//                 { expiresIn: "1d" }
+//             );
+            
+//             res.cookie("token", token, {
+//             httpOnly: true,
+//             secure: process.env.NODE_ENV === "production", // only secure in production
+//             sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+//             maxAge: 24 * 60 * 60 * 1000,
+//         });
+
+
+//             return res.status(200).json({
+                
+//                 message: "User logged in successfully",
+//                 user: {
+//                     id: user._id,
+//                     username: user.username,
+//                 },
+//                 token
+//             });
+//         }
+
+//         export async function me(req,res){
+//             const token =req.cookies.token
+            
+//             if (!token){
+                
+//                 return res.status(401).json({
+//                     message: "Unauthorized"
+//                 });
+//             }
+//             try {
+//                const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+//                 const user = await userModel.findById(decoded.id);
+
+//                 return res.status(200).json({
+//                 user: {
+//                     id: user._id,
+//                     username: user.username,
+//                 },
+//                 message: "User authenticated successfully"
+//                 });
+
+//     }
+//             catch (error) {
+//                 return res.status(401).json({
+//                     message: "unauthorized",
+//                 });
+//             }
+//         }
+            
 import userModel from "../models/user.model.js" 
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
@@ -28,12 +150,13 @@ export async function registerUser(req,res){
              process.env.JWT_SECRET_KEY,
             {expiresIn: "1d"})    //1day
 
-                 res.cookie("token", token, {
+            res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production", // only secure in production
             sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
             maxAge: 24 * 60 * 60 * 1000,
         });
+
             //set cookie in response
 
         //Response send krne se phle token generate krege 
@@ -72,7 +195,7 @@ export async function registerUser(req,res){
                 { expiresIn: "1d" }
             );
             
-            res.cookie("token", token, {
+           res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production", // only secure in production
             sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
@@ -119,4 +242,3 @@ export async function registerUser(req,res){
                 });
             }
         }
-            
